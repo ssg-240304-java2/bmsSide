3284d2f311146eb43b2cc693d51898930ea0bc96
jisoo78 <ekelldks@naver.com>
2024-06-03 18:30:33 +0900

    #7 주문도서 선택 테이블출력 완료

---
+++ bmsSide/src/main/java/com/side/bms/common/MyBatisTemplate.java
@@ -0,0 +1,27 @@
+package com.side.bms.common;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+
+public class MyBatisTemplate {
+
+    private static SqlSessionFactory sqlSessionFactory;
+    static {
+        String resource = "config/mybatis-config.xml"; // resources폴더 하위경로
+        try {
+            sqlSessionFactory =  new SqlSessionFactoryBuilder().build(
+                    Resources.getResourceAsStream(resource));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static SqlSession getSqlSession(){
+        return sqlSessionFactory.openSession(false);
+    }
+
+}
